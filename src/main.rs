9f50f8636b2b3a82b8c568fb51821ad4361f7d98
Star Brilliant <coder@poorlab.com>
2025-07-25 17:54:57 +0000

    Improve logging

--- src/main.rs
+++ src/main.rs
@@ -560,11 +560,10 @@ async fn on_message(
 
 // https://spec.matrix.org/v1.14/client-server-api/#mroomencrypted
 #[instrument(skip_all)]
-async fn on_utd(event: OriginalSyncRoomEncryptedEvent, room: Room, raw_event: RawEvent) {
+async fn on_utd(_event: OriginalSyncRoomEncryptedEvent, room: Room, raw_event: RawEvent) {
     error!(
-        "Unable to decrypt room {}, event {} ({}).",
+        "Unable to decrypt: room {}, event {}",
         room.room_id(),
-        event.event_id,
         raw_event.get()
     );
 }
